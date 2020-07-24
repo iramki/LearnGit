13c5f452dd4e241acab4587b0db2382af7fcf8d7
Ramkishan <ramkishan@PROFINCHBLR.COM>
2020-07-24 12:44:27 +0530

    first commit

---
+++ src/test/java/com/ramki/gitexample/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramki.gitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
